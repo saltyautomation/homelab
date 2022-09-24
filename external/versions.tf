7a3b14742fbe085b071046cf0638ffc4a2f2192b
Ty Morton <ty@tmorton.net>
2022-09-24 01:33:33 +0000

    updated metal config

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "tmorton"
 
     workspaces {
       name = "homelab-external"
